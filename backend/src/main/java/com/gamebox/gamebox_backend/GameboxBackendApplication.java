94b90924dfa8c00eeff86fcfec2147ba09622a90
Zvenytoh <mehdienh@icloud.com>
2025-10-24 16:18:16 +0200

    Initial commit

---
+++ backend/src/main/java/com/gamebox/gamebox_backend/GameboxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gamebox.gamebox_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameboxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameboxBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gamebox/gamebox_backend/GameboxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamebox.gamebox_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameboxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
